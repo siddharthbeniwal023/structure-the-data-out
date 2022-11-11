89b7e9767e2be9a6c65f91fd380918bb4a08c46e
siddharthbeniwal023 <siddharthbeniwal2001@gmail.com>
2022-11-11 12:10:13 +0530

    added comments to quick sort

--- Sortings/Quicksort/quicksort.cpp
+++ Sortings/Quicksort/quicksort.cpp
@@ -3,15 +3,24 @@
 using namespace std;
 
 int partition(vector<int> &arr,int start,int end){
+
+    // make the first element pivot
     int pivot=arr[start];
     int count=0;
+    
+    // count the number of elements smaller than pivot
     for(int i=start+1;i<=end;i++){
         if(arr[i]<=pivot)
             count++;
     }
+
+    //specify the pivot index
     int pivotindex=start+count;
+
+    //swap it with the current element at pivotindex
     swap(arr[pivotindex],arr[start]);
     
+    // make the sorting as left side (<a) & right side (>a)
     int i=start,j=end;
     while(i<pivotindex && j>pivotindex){
         while(arr[i]<=pivot)
@@ -31,9 +40,11 @@ void quicksort(vector<int> &arr,int start,int end){
         return;
 
     int p = partition(arr,start,end);
-
+    
+    // sort the left side of pivot 
     quicksort(arr,start,p-1);
 
+    // sort the right side of pivot
     quicksort(arr,p+1,end);
 
 }
